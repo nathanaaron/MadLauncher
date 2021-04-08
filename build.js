2e3afe50adf2c9ccb3d9024130a0abc6ad6bf30c
nathanaaron <nathan@naaron.com>
2021-04-07 23:09:56 -0500

    Updated app-update.yml

--- build.js
+++ build.js
@@ -18,8 +18,8 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'MadLauncher',
+        productName: 'MadLads Launcher',
         artifactName: '${productName}-setup-${version}.${ext}',
         copyright: 'Copyright © 2018-2020 Daniel Scalzi',
         directories: {
@@ -47,7 +47,7 @@ builder.build({
         linux: {
             target: 'AppImage',
             maintainer: 'Daniel Scalzi',
-            vendor: 'Daniel Scalzi',
+            vendor: 'nathanaaron',
             synopsis: 'Modded Minecraft Launcher',
             description: 'Custom launcher which allows users to join modded servers. All mods, configurations, and updates are handled automatically.',
             category: 'Game'
